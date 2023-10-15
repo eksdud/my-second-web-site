53622a0ef658a076abc43492545317c451c89ce9
김단영 <128346309+eksdud@users.noreply.github.com>
2023-10-15 20:43:28 +0900

    Add files via upload

---
+++ w05/master.js
@@ -0,0 +1,24 @@
+var Body = {
+    setBackgroundColor: function(color){document.querySelector('body').style.backgroundColor=color;},
+    setColor: function(color){document.querySelector('body').style.color=color;}
+}
+
+var Links = {
+    setColor: function(color){
+        var i = 0;
+        var alist=document.querySelectorAll('a');
+        while (i<alist.length){
+            alist[i].style.color=color;
+            i=i+1;}}
+}
+
+function nightDayHandler(self){
+    if(self.value=='night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        Links.setColor('powderblue');
+        self.value='day';}
+    else{Body.setBackgroundColor('white');
+        Body.setColor('black');
+        Links.setColor('blue');
+        self.value='night';}}
